f8393121cf751e441d5adf32cdd66f572d8e0ed7
Ka Sen Low <lowkasen@gmail.com>
2025-07-31 22:02:31 +0800

    feat: initial commit

---
+++ grafana/install-node-exporter.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -euo pipefail
+
+## Node exporter install script
+# Prompt for hostname
+read -p "Enter the hostname to set for this machine: " NEW_HOSTNAME
+
+# Replace the placeholder in the script
+sudo hostnamectl set-hostname "$NEW_HOSTNAME"
+
+# System user for node_exporter
+if ! id "node_exporter" &>/dev/null; then
+  sudo useradd -rs /bin/false node_exporter
+fi
+
+# Download Node Exporter
+curl -LO https://github.com/prometheus/node_exporter/releases/latest/download/node_exporter-1.9.1.linux-amd64.tar.gz
+
+# Extract and install
+tar xvf node_exporter-1.9.1.linux-amd64.tar.gz
+cd node_exporter-1.9.1.linux-amd64
+sudo cp node_exporter /usr/local/bin/
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+# Create systemd service
+sudo tee /etc/systemd/system/node_exporter.service > /dev/null <<EOF
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=default.target
+EOF
+
+# Reload systemd to recognize the new service
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
+
+# Verify installation
+if systemctl status node_exporter | grep -q "active (running)"; then
+  echo "Node Exporter installed and running successfully."
+else
+  echo "Node Exporter installation failed."
+fi
+
+### Networking security
+# Open up port 9100 to the remote prometheus server. For now its 18.141.250.212/32
+# Or leave it for a local prometheus server to scrape localhost:9100
---
+++ grafana/install-prometheus.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -euo pipefail
+
+## Prometheus install script
+PROMETHEUS_VERSION="3.5.0"
+PROMETHEUS_FILENAME="prometheus-${PROMETHEUS_VERSION}.linux-amd64"
+PROMETHEUS_TAR="${PROMETHEUS_FILENAME}.tar.gz"
+
+# Set up Prereqs
+if ! id "prometheus" &>/dev/null; then
+  sudo useradd -rs /bin/false prometheus
+fi
+sudo mkdir -p /etc/prometheus /var/lib/prometheus
+sudo chown -R prometheus:prometheus /etc/prometheus /var/lib/prometheus
+
+# Download and extract prometheus binary
+curl -LO "https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/${PROMETHEUS_TAR}"
+tar xvf "${PROMETHEUS_TAR}"
+cd "${PROMETHEUS_FILENAME}"
+
+# Set binaries
+sudo cp prometheus promtool /usr/local/bin/
+# sudo cp -r consoles console_libraries /etc/prometheus/
+sudo cp prometheus.yml /etc/prometheus/
+sudo chown -R prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/prometheus /usr/local/bin/promtool
+
+# Create prometheus.yaml file in /etc/prometheus/prometheus.yml
+sudo tee /etc/prometheus/prometheus.yml > /dev/null <<EOF
+global:
+  scrape_interval: 15s
+
+scrape_configs:
+    - job_name: node
+      static_configs:
+        - targets:
+            - 'localhost:9100'
+EOF
+
+# Create systemd service
+sudo tee /etc/systemd/system/prometheus.service > /dev/null <<EOF
+[Unit]
+Description=Prometheus Monitoring
+After=network.target
+
+[Service]
+User=prometheus
+Group=prometheus
+ExecStart=/usr/local/bin/prometheus \\
+  --config.file=/etc/prometheus/prometheus.yml \\
+  --storage.tsdb.path=/var/lib/prometheus/ \\
+  --web.console.templates=/etc/prometheus/consoles \\
+  --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=default.target
+EOF
+
+# Start service
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl start prometheus
+sudo systemctl enable prometheus
+
+# Verify installation
+if systemctl status prometheus | grep -q "active (running)"; then
+  echo "Prometheus installed and running successfully."
+else
+  echo "Prometheus installation failed."
+fi
