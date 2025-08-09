6a094bd9faa43445ae39160fa28676e885e9686b
Ka Sen Low <lowkasen@gmail.com>
2025-08-10 00:30:11 +0800

    feat: updated hello.sh

--- grafana/hello.sh
+++ grafana/hello.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-read -p "Enter something: " input
+# read -p "Enter something: " input
+
+# Force reading from the terminal, not the pipe
+printf "Enter something: "
+IFS= read -r input < /dev/tty
+
 echo "You entered: $input"
